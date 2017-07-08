3ea2d5f41e8717980e753db35c0c7be1f0b08346
Pedro Sland <pedrosland@gmail.com>
2017-07-08 10:27:52 +0100

    Initial bindings for some of mmal

---
+++ build.rs
@@ -0,0 +1,41 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to tell rustc to link the system bzip2
+    // shared library.
+    println!("cargo:rustc-link-lib=mmal_core");
+    println!("cargo:rustc-link-lib=mmal_util");
+    println!("cargo:rustc-link-lib=mmal_vc_client");
+    println!("cargo:rustc-link-lib=vcos");
+    println!("cargo:rustc-link-lib=bcm_host");
+    println!("cargo:rustc-link-search=native=/opt/vc/lib");
+//    println!("cargo:include=/opt/vc/include/interface");
+//    println!("cargo:libdir=/opt/vc/lib");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // Do not generate unstable Rust code that
+        // requires a nightly rustc and enabling
+        // unstable features.
+//        .unstable_rust(false)
+//      // Fix library path to include mmal headers
+        .clang_arg("-I/opt/vc/include")
+        // The input header we would like to generate
+        // bindings for.
+        .header("wrapper.h")
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ wrapper.h
@@ -0,0 +1,7 @@
+#include <interface/mmal/mmal.h>
+#include <interface/mmal/util/mmal_default_components.h>
+
+// These are hacks to make the linker link
+#include <bcm_host.h>
+#include <interface/mmal/vc/mmal_vc_api.h>
+#include <interface/vcos/vcos.h>
