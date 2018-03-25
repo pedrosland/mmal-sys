58a9cbee580de3f7264577cc98ad01a64a8a76ce
Travis CI <>
2018-03-25 20:16:14 +0000

    Deploy to GitHub Pages: 622776df47d9cc6440a3cf24e30d1bf0f2fc51ce

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["mmal_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"mmal_sys/struct.MMAL_PARAMETER_CAMERA_INFO_CAMERA_T.html\" title=\"struct mmal_sys::MMAL_PARAMETER_CAMERA_INFO_CAMERA_T\">MMAL_PARAMETER_CAMERA_INFO_CAMERA_T</a>",synthetic:false,types:["mmal_sys::MMAL_PARAMETER_CAMERA_INFO_CAMERA_T"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"mmal_sys/struct.MMAL_PARAMETER_CAMERA_INFO_T.html\" title=\"struct mmal_sys::MMAL_PARAMETER_CAMERA_INFO_T\">MMAL_PARAMETER_CAMERA_INFO_T</a>",synthetic:false,types:["mmal_sys::MMAL_PARAMETER_CAMERA_INFO_T"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
